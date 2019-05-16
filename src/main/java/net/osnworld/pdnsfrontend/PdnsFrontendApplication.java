67d3d4de433f055ea09b83e5f0aea6feefeb7ac5
Maisonneuve Hervé <48790547+hmaisonneuve6513@users.noreply.github.com>
2019-05-16 15:59:30 +0200

    first upload

---
+++ src/main/java/net/osnworld/pdnsfrontend/PdnsFrontendApplication.java
@@ -0,0 +1,13 @@
+package net.osnworld.pdnsfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdnsFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdnsFrontendApplication.class, args);
+	}
+
+}
