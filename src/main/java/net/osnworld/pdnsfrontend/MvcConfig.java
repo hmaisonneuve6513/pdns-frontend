4d04b87472d2d647f802e690a7dd07572ecfae13
herve <hmaisonneuve6513@gmail.com>
2019-05-16 23:33:01 +0200

    init application port

--- src/main/java/net/osnworld/pdnsfrontend/MvcConfig.java
+++ src/main/java/net/osnworld/pdnsfrontend/MvcConfig.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/home.html").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/hello.html").setViewName("hello");
+        registry.addViewController("/login.html").setViewName("login");
     }
 }
