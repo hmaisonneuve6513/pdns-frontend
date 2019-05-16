5e58133e1d9be570c8e193fdc9cb7813d0044d9a
herve <hmaisonneuve6513@gmail.com>
2019-05-16 23:46:25 +0200

    init application port

--- src/main/java/net/osnworld/pdnsfrontend/WebSecurityConfig.java
+++ src/main/java/net/osnworld/pdnsfrontend/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .loginPage("/login")
+                    .loginPage("/login.html")
                     .permitAll()
                     .and()
                 .logout()
